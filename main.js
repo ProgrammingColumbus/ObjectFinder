7e6950d1c0ab62a2abfbf616da747f9a9156332b
ProgrammingColumbus <103267295+ProgrammingColumbus@users.noreply.github.com>
2022-12-30 10:45:38 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 //Start
 
 status1 = "";
-
-
-
-//Preload
-function preload(){
-}
+objects = [];
+object_name = document.getElementById("textbox").value;
 
 
 //Setup
@@ -27,12 +23,36 @@ function modelLoaded(){
 
 //Got Result
 function gotResult(){
-
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 
 //Draw
 function draw(){
+    if(status1 != ""){
+        objectDetector.detect(video,gotResult);
+        for(i = 0; i< objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected.";
+            document.getElementById("numberObjects").innerHTML = "Number of Objects Detected: " + objects.length;
+
+            fill("#A097FF");
+            percent = floor(objects[i].confidence * 100);
+            text(objetcs[i].label + " " + percent + "% ", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#86DA09");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+        if(objects[i].label == object_name){
+            document.getElementById("objectStatus").innerHTML = "Object Found!";
+        }
+        else{
+            document.getElementById("objectStatus").innerHTML = "Object Not Found!";
+        }
+    }
 }
 
 
